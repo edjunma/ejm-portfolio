dba53a499b2be480450d7e0d37f569984edbcce0
edjunma <junwenma@buffalo.edu>
2019-05-02 06:06:09 -0400

    Fixed smooth scroll script, added jquery backup

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,3 +1,20 @@
-const scroll = new SmoothScroll('.navbar a[href*="#"]', {
-  speed: 800
-});
+// // jQuery Smooth Scroll
+// $("#navbar a").on("click", function(e) {
+// 	if (this.hash !== "") {
+// 		e.preventDefault()
+
+// 		const hash = this.hash
+
+// 		$("html, body").animate(
+// 			{
+// 				scrollTop: $(hash).offset().top
+// 			},
+// 			800
+// 		)
+// 	}
+// })
+
+// Smooth Scroll Script
+const scroll = new SmoothScroll('#navbar a[href*="#"]', {
+	speed: 800
+})
